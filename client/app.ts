8bbeca1d56ce1002e5c7b3aaf641f0051c84d34c
jwel <whdgnl1123@gmail.com>
2019-08-15 20:17:40 +0900

    rename topic and change logging source from $SYS/# topic to callback

--- client/app.ts
+++ client/app.ts
@@ -1,16 +1,18 @@
 import * as mqtt from 'mqtt';
 let client = mqtt.connect('mqtt://localhost:1883');
- 
+
 client.on('connect', function () {
-  client.subscribe('presence', function (err) {
-    if (!err) {
-      client.publish('presence', 'Hello mqtt')
-    }
-  })
-})
- 
+  console.log('connected');
+  client.subscribe('general/echo/pong', function (err) {
+    if (!err)
+      client.publish('general/echo/ping', 'Hello mqtt');
+  });
+});
+
 client.on('message', function (topic, message) {
-  // message is Buffer
-  console.log(message.toString())
-  client.end()
-})
+  if (topic === 'general/echo/pong') {
+    // message is Buffer
+    console.log('pong message:', message.toString());
+  }
+  client.end();
+});
--- server/app.ts
+++ server/app.ts
@@ -1,34 +1,38 @@
 import * as mosca from 'mosca';
 
 let settings = {
-    port: 1883,
-    persistence: mosca.persistence.Memory
+  port: 1883,
+  persistence: mosca.persistence.Memory
 };
-
 
 var server = new mosca.Server(settings, function() {
-    console.log('Mosca server is up and running')
+  console.log('Mosca server is up and running');
 });
-
 server.on('clientConnected', function(client: any) {
-  console.log('client connected', client.id);
+  console.log('client connected', client.id)
+});
+server.on("clientDisconnected", function(client: any) {
+  console.log('client disconnected', client.id)
 });
 
+server.on('published', function(packet, client) {
+  if (packet.topic.indexOf('$SYS') === 0) return;
 
-server.published = function(packet: any, client: any, cb: any) {
-    if (packet.topic.indexOf('echo') === 0) {
-    console.log('ON PUBLISHED', packet.payload.toString(), 'on topic', packet.topic);
-    return cb();
-    }
+  // console.log('raw packet:', packet)
+  console.log('on topic \'' + packet.topic + '\', payload:', packet.payload);
+
+  if (packet.topic.indexOf('general/echo/ping') === 0) {
+    console.log('ping message:', packet.payload.toString());
 
     var newPacket = {
-    topic: 'echo/' + packet.topic,
-    payload: packet.payload,
-    retain: packet.retain,
-    qos: packet.qos
+      topic: 'general/echo/pong',
+      payload: packet.payload,
+      retain: packet.retain,
+      qos: packet.qos
     };
 
-    console.log('newPacket', newPacket);
-
-    server.publish(newPacket, cb);
-};
+    server.publish(newPacket, function() {
+      console.log('pong published');
+    });
+  }
+});
