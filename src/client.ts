dd9c7e9c822188ec1f5942ec65d7df73622f6be8
chanhomin <chan02468@naver.com>
2019-08-17 17:32:31 +0900

    TLS setup

--- src/client.ts
+++ src/client.ts
@@ -1,5 +1,31 @@
 import * as mqtt from 'mqtt';
-let client = mqtt.connect('mqtt://localhost:1883');
+import * as fs from 'fs';
+import * as path from 'path';
+
+var KEY = fs.readFileSync(path.join(__dirname, '/../client.key'))
+var CERT = fs.readFileSync(path.join(__dirname, '/../client.crt'))
+var CA = fs.readFileSync(path.join(__dirname, '/../root.crt'))
+
+var PORT = 8443
+var HOST = 'localhost'
+
+var options = {
+  host: 'localhost',
+    port: 8443,
+    protocol: 'mqtts',
+    protocolId: 'MQIsdp',
+    protocolVersion: 3,
+    secureProtocol: 'TLSv1_method',
+    reconnectPeriod: 5 * 1000,
+    rejectUnauthorized: true,
+    connectTimeout: 10 * 1000,
+    ca: [CA],
+    key: KEY,
+    cert: CERT
+}
+
+var client = mqtt.connect(options);
+
 
 client.on('connect', function () {
   console.log('connected');
--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,44 @@
 import * as mosca from 'mosca';
 
+let SECURE_KEY = __dirname + '/../server.key';
+let SECURE_CERT = __dirname + '/../server.crt';
+
 let settings = {
   port: 1883,
-  persistence: mosca.persistence.Memory
+  // persistence: mosca.persistence.Memory,
+  secure : {
+    port: 8443,
+    keyPath: SECURE_KEY,
+    certPath: SECURE_CERT,
+  }
 };
 
-var server = new mosca.Server(settings, function() {
+let server = new mosca.Server(settings, function() {
   console.log('Mosca server is up and running');
+});
+
+// Accepts the connection if the username and password are valid
+let authenticate = function(client:any, username:any, password:any, callback:any) {
+  let authorized = (username === 'alice' && password.toString() === 'secret');
+  if (authorized) client.user = username;
+  callback(null, authorized);
+}
+
+// In this case the client authorized as alice can publish to /users/alice taking
+// the username from the topic and verifing it is the same of the authorized user
+let authorizePublish = function(client:any, topic:any, payload:any, callback:any) {
+  callback(null, client.user == topic.split('/')[1]);
+}
+
+// In this case the client authorized as alice can subscribe to /users/alice taking
+// the username from the topic and verifing it is the same of the authorized user
+let authorizeSubscribe = function(client:any, topic:any, callback:any) {
+  callback(null, client.user == topic.split('/')[1]);
+}
+server.on('ready', function() {
+  server.authenticate = authenticate;
+  server.authorizePublish = authorizePublish;
+  server.authorizeSubscribe = authorizeSubscribe;
 });
 server.on('clientConnected', function(client: any) {
   console.log('client connected', client.id)
@@ -24,7 +56,7 @@ server.on('published', function(packet: any, client: any) {
   if (packet.topic.indexOf('general/echo/ping') === 0) {
     console.log('ping message:', packet.payload.toString());
 
-    var newPacket = {
+    let newPacket = {
       topic: 'general/echo/pong',
       payload: packet.payload,
       retain: packet.retain,
